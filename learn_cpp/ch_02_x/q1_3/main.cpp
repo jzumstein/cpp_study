9479d05bc271f16b538399a3603d0d9349454767
jzumstein <zumsteinpromotion@gmail.com>
2024-10-16 20:55:52 -0500

    iomanip library practice

--- learn_cpp/ch_02_x/q1_3/main.cpp
+++ learn_cpp/ch_02_x/q1_3/main.cpp
@@ -1,9 +1,13 @@
 #include "io.h"
+#include <iomanip>
 #include <iostream>
 
 int main()
 {
-	int u_num_x{readNumber()};
-	int u_num_y{readNumber()};
-	writeAnswer(u_num_x + u_num_y);
+	//int u_num_x{readNumber()};
+	//int u_num_y{readNumber()};
+	//writeAnswer(u_num_x + u_num_y);
+	std::cout << std::left;
+	std::cout << std::setw(16) << "Fruits:" << "Apples, Bananas, Cherries\n";
+	std::cout << std::setw(16) << "Vegetables:" << "Asperagus, Blumen Onion, Cauliflower\n";
 }
