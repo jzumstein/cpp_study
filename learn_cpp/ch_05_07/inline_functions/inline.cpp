82e33f4b64f7d2fa1b623f75c17de447d049cb36
jzumstein <zumsteinpromotion@gmail.com>
2024-11-20 22:20:15 -0600

    Updating inline modulo

--- learn_cpp/ch_05_07/inline_functions/inline.cpp
+++ learn_cpp/ch_05_07/inline_functions/inline.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
 	std::cout << ((5 < 6) ? 5 : 6) << "\n";
 	std::cout << ((12 > 56) ? 12 : 56) << "\n";
+
+	std::string modTwo{ "NO WAYYYY!" };
+	std::string modThree{ "Yes WAYYY!" };
+	int res = ((5 % 2) < (18 % 3) ? 0 : 1);
+	std::string oString{};
+
+	if (res > 0) {
+		oString += modTwo;
+	}
+	else
+	{
+		oString += modThree;
+	}
+
+	std::cout << oString;
 	return 0;
 }
